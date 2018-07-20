3a701544d54d582bc0490c1e6d9a62db364c903e
Alex Mireles <anchises1968@gmail.com>
2018-07-20 14:55:34 -0700

    fixed buildspec

--- js/menu.js
+++ js/menu.js
@@ -19,7 +19,7 @@ const RoutePath = () => (
 
 const Menu = () => (
   <header>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <nav className="navbar navbar-expand-lg custom-color fixed-top">
       <div className="container">
         <a className="navbar-brand" href="#"></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
